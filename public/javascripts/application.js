27df226496bcfe71214b0d692b527254c3f60d31
Chris Cheshire <chris.cheshire@gmail.com>
2012-05-21 15:08:09 +0100

    added js to support landscape mode on orientation

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,4 +1,14 @@
 $(document).ready(function() {
+  
+  if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i)) {
+    var viewportmeta = document.querySelector('meta[name="viewport"]');
+    if (viewportmeta) {
+        viewportmeta.content = 'width=device-width, minimum-scale=1.0, maximum-scale=1.0, initial-scale=1.0';
+        document.body.addEventListener('gesturestart', function () {
+            viewportmeta.content = 'width=device-width, minimum-scale=0.25, maximum-scale=1.6';
+        }, false);
+    }
+  }
 
 	// Open external links in a new window
 	hostname = window.location.hostname
